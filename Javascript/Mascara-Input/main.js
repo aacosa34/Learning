a461ad83794092e68f9c15adc9ed3e6572ec8efb
Adrian Acosa Sanchez <adrianacosa@correo.ugr.es>
2022-07-12 10:50:51 +0200

    Mascara para Input

---
+++ Javascript/Mascara-Input/main.js
@@ -0,0 +1,8 @@
+const inputCard = document.querySelector("#inputCard");
+const inputDate = document.querySelector("#inputDate");
+const inputCVV = document.querySelector("#inputCVV");
+
+// Las almoadillas se pueden sustituir por cualquier caracter que queramos
+const maskNumber = "####-####-####-####";
+const maskDate = "##/##";
+const maskCVV = "###";
